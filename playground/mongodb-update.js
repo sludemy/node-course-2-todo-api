f96cdd97ba27c1cb8b79f80bab052d26ec744194
anakin <seanlittlechildudemy@gmail.com>
2018-08-31 18:09:49 +1000

    update

---
+++ playground/mongodb-update.js
@@ -0,0 +1,23 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp')
+
+    db.collection('Todos').findOneAndUpdate({
+        _id: new ObjectID("5b88f4565627753dd0e67b5a")
+    }, {
+        $set: {
+            completed: true
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+    // client.close();
+});
